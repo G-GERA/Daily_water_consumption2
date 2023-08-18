c5f8c1f07950c91d078e374a7f4c1b04e048c447
G-GERA <141760379+G-GERA@users.noreply.github.com>
2023-08-18 17:13:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,213 @@
+var show = 0;
+var hide = 0;
+var image1=["empty.png", "full.png"]
+
+function plus()
+{
+  show++;
+  if(show==1)
+  {
+  document.getElementById('cup5').style.visibility = 'visible';
+  }
+  
+  if(show==2)
+  {
+  document.getElementById('cup6').style.visibility = 'visible';
+  }  
+  
+  if(show==3)
+  {
+  document.getElementById('cup7').style.visibility = 'visible';
+  }
+    
+  if(show==4)
+  {
+  document.getElementById('cup8').style.visibility = 'visible';
+  }  
+  
+  if(show==5)
+  {
+  document.getElementById('cup9').style.visibility = 'visible';
+  }
+    
+  if(show==6)
+  {
+  document.getElementById('cup10').style.visibility = 'visible';
+  }
+  
+}
+
+var i=0;
+function cup1(){
+  if(i == 2)
+     {
+       i=0;
+     }
+if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+{
+  alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+  document.getElementById('badge1').style.visibility = 'visible';
+ }
+
+    document.getElementById("cup1").src =image1[i] ;
+  i++;
+  console.log(i)
+ 
+}
+
+var a=0;
+function cup2(){
+  if(a == 2)
+     {
+       a=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup2").src =image1[a] ;
+  a++;
+ 
+}
+
+var b=0;
+function cup3(){
+  if(b == 2)
+     {
+       b=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup3").src =image1[b] ;
+  b++;
+ 
+}
+
+var c=0;
+function cup4(){
+  if(c == 2)
+     {
+       c=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup4").src =image1[c] ;
+  c++;
+ 
+}
+
+var d=0;
+function cup5(){
+  if(d == 2)
+     {
+       d=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup5").src =image1[d] ;
+  d++;
+ 
+}
+
+var e=0;
+function cup6(){
+  if(e == 2)
+     {
+       e=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup6").src =image1[e] ;
+  e++;
+ 
+}
+
+var f=0;
+function cup7(){
+  if(f == 2)
+     {
+       f=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup7").src =image1[f] ;
+  f++;
+ 
+}
+
+var g=0;
+function cup8(){
+  if(g == 2)
+     {
+       g=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup8").src =image1[g] ;
+  g++;
+ 
+}
+
+var h=0;
+function cup9(){
+  if(h == 2)
+     {
+       h=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup9").src =image1[h] ;
+  h++;
+ 
+}
+
+var j=0;
+function cup10(){
+  if(j == 2)
+     {
+       j=0;
+     }
+     if(a>=1 && b>=1 && c>=1 && d>=1 && e>=1 && f>=1 && g>=1 && h>=1 && i>=1 && j>=1)
+     {
+      alert("Congratulations!!! YOU COMPLETED THE AUGUST CHALLENGE!! :D You have now earned a badge!! Scroll down to see it!");
+      document.getElementById('badge1').style.visibility = 'visible';
+     }
+    
+    document.getElementById("cup10").src =image1[j] ;
+  j++;
+
+
+ 
+}
+
+
